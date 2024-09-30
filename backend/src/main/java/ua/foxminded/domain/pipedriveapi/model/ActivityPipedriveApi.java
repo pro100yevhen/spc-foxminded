28c215623752e8ef5c4d817382ab8cbe0f473540
yevheniipiddubnyi <piddubni2@gmail.com>
2024-09-30 18:56:37 +0300

    update activity for webhook v2

--- backend/src/main/java/ua/foxminded/domain/pipedriveapi/model/ActivityPipedriveApi.java
+++ backend/src/main/java/ua/foxminded/domain/pipedriveapi/model/ActivityPipedriveApi.java
@@ -1,5 +1,35 @@
 package ua.foxminded.domain.pipedriveapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ActivityPipedriveApi {
 
+    private boolean success;
+    private Data data;
+    @JsonProperty("related_objects")
+    private RelatedObjects relatedObjects;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(final boolean success) {
+        this.success = success;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(final Data data) {
+        this.data = data;
+    }
+
+    public RelatedObjects getRelatedObjects() {
+        return relatedObjects;
+    }
+
+    public void setRelatedObjects(final RelatedObjects relatedObjects) {
+        this.relatedObjects = relatedObjects;
+    }
 }
